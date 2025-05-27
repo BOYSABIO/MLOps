e89435815aa63851c0b5b6d1539f2b1484a9e92b
brunnerlucas <lucas.brunner@student.ie.edu>
2025-05-28 00:32:04 +0200

    adding features and preprocessing with logs and tests

---
+++ MNIST/src/features.py
@@ -0,0 +1,8 @@
+import logging
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+def flatten_images(x):
+    logger.info("Flattening images to vectors of shape")
+    return x.reshape((x.shape[0], -1))
